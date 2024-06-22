e1208b9f405cfe910ee3d9a357d734ba3ed7f816
amantyagi23 <amantyagi4987@gmail.com>
2024-06-22 17:28:27 +0530

    21june

---
+++ ForLoop.java
@@ -0,0 +1,11 @@
+/**
+ * ForLoop
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            System.out.println("Hello Bhavya" + i);
+        }
+    }
+}
