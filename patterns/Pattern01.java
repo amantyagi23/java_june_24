dc874df541a1cf3c10e61dae5080ff317313ae6d
amantyagi23 <amantyagi4987@gmail.com>
2024-06-27 17:31:19 +0530

    27june

---
+++ patterns/Pattern01.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Pattern01 {
+    public static void main(String[] args) {
+        int n = 5;
+        // tow - down
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
