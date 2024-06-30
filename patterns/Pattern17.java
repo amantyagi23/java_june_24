20a019015730c799be9b61e234ab3998436be9cd
amantyagi23 <amantyagi4987@gmail.com>
2024-06-30 16:06:48 +0530

    30june

--- patterns/Pattern17.java
+++ patterns/Pattern17.java
@@ -4,9 +4,16 @@ public class Pattern17 {
     public static void main(String[] args) {
         int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print("* ");
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i + 1; j++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
             }
             System.out.println();
         }
