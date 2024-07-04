d28e4db4aabc7a5b19a8b3fe83540214f2287f1c
amantyagi23 <amantyagi4987@gmail.com>
2024-07-04 17:44:55 +0530

    4july

---
+++ array/ArrayExample.java
@@ -0,0 +1,38 @@
+
+package array;
+
+import java.util.Scanner;
+
+/**
+ * ArrayExample
+ */
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // data sturcture
+        int a = 9;
+
+        int array[] = { 444, 67, 87, 90, 100, 3, 8, 11, 9 };
+
+        // index = 0 to array.length-1
+
+        System.out.println(array[0]);
+
+        array[0] = 7;
+        array[1] = 325698768;
+        array[2] = 6000;
+        array[3] = 87654;
+        array[4] = 777;
+        array[7] = 8888;
+
+        System.out.println(array[0]);
+
+        int tushar[] = new int[5];
+
+        int b = 10;
+        int c = 12;
+
+    }
+}
